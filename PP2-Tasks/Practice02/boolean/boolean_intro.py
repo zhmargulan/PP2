70f73344b967fd271417ee04c074917e86590617
Margulan <margo220408@icloud.com>
2026-01-28 21:48:54 +0500

    prac02

---
+++ PP2-Tasks/Practice02/boolean/boolean_intro.py
@@ -0,0 +1,21 @@
+# 1
+print(True)
+print(False)
+
+#2
+print(bool("Hello"))
+print(bool(15))
+
+#3
+print(bool("abc"))
+print(bool(123))
+print(bool(["apple", "cherry", "banana"]))
+
+#4
+print(bool(False))
+print(bool(None))
+print(bool(0))
+print(bool(""))
+print(bool(()))
+print(bool([]))
+print(bool({}))
